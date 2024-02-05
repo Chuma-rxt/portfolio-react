fe62563da56f1d1de8ffd5edb07be87e57c0f687
Chuma-rxt <craxoti60@gmail.com>
2024-02-05 16:09:29 +0200

    app

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -3,13 +3,13 @@ import "./HeroImgStyles.css";
 
 import React from "react";
 
-import homeImg from "../assets/WhatsApp Image 2024-01-25 at 11.11.38.jpeg"
+import HomeImg from "../assets/homeImg.jpeg";
 
 const HeroImg = () => {
   return 
     <div className= "hero">
         <div className='mask'>
-           <img className="into-img" src="homeImg"/>
+           <img className="homeImg" src={HomeImg} alt="HomeImg"/>
         </div>
     </div>
   

c92972950faa21ca4f0fd2db84c618f0822f9d5d
Chuma-rxt <craxoti60@gmail.com>
2024-02-06 15:43:51 +0200

    app

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import NavBar from "../components/NavBar";
 import  Footer from "../components/Footer";
+import HeroImg2 from '../components/HeroImg2';
 
 const Project = () => {
   return (
     <div>
       <NavBar />
+      <HeroImg2 />
       <Footer />
     </div>
   )

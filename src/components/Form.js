8c8a1831bfe10d97747e8f529d6ba0871302e11c
Chuma-rxt <craxoti60@gmail.com>
2024-02-09 09:42:00 +0200

    created a form

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,6 +7,16 @@ const Form = () => {
     <div className="form">
         <form>
             <label>Your Name</label>
+            <input type="text"></input>
+
+            <label>Email</label>
+            <input type="email"></input>
+
+            <label>Subject</label>
+            <input type="text"></input>
+
+            <label>Message</label>
+            <textarea rows="6" placeholder="Type your message here" />
         </form>
     </div>
   )

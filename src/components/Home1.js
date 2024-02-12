2c7d53922c594d133e97b53f4131bc419a16d85d
Chuma-rxt <craxoti60@gmail.com>
2024-02-12 13:04:50 +0200

    creating slide show

--- src/components/Home1.js
+++ src/components/Home1.js
@@ -1,10 +1,25 @@
 import "./Home1Styles.css";
 
 import React from 'react'
+import Cis from "../assets/Cisco-Courses.jpg"
 
 const Home1 = () => {
   return (
-    <div ></div>
+    <div className="slide-container">
+        <div className="slide-content">
+            <div className="card-wrapper">
+                <div className="card">
+                    <div className="image-content">
+                        <span className="overlay"></span>
+
+                        <div className="card-image">
+                            <img src={Cis} className="img-card" alt="true" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import  HeroImg from "../components/HeroImg";
 import  Footer from "../components/Footer";
+import Home1 from "../components/Home1";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
       <HeroImg />
+      <Home1 />
       <Footer />
     </div>
   )

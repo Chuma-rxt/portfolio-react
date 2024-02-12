d8f0aba404c2a657ea0429e401ab54800c2db5fe
Chuma-rxt <craxoti60@gmail.com>
2024-02-12 12:40:04 +0200

    updated my form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,101 +2,24 @@ import "./FormStyles.scss";
 
 import React from 'react';
 
-function Form () {
-
-    const handleSubmit = () => {
-
-    }
-    const handleInputChange = () =>{}
+const Form = () => {
   return (
-    <section className="contact-section">
-        <div className="container">
-            <div className="row justify-=content-center">
-                <div className="col-md-10">
-                    <div className="row no-gutters">
-                        <div className="col-md-6">
-                            <div className="contact-wrap w-2100 p-lg-5 p-4">
-                                <h2 className="mb-4">Send Me a message</h2>
-                                <form id="contactForm" className="contactForm" onSubmit={handleSubmit}>
-                                    <div className="row">
-                                        <div className="col-md-12">
-                                            <div className="form-group">
-                                                <input type="text" className="form-control" name="name" placeholder="Name" onChange={handleInputChange}/>
-                                            </div>
-                                        </div>
+    <div className="form">
+        <form>
+            <label>Your Name</label>
+            <input type="text"></input>
 
-                                        <div className="col-md-12">
-                                            <div className="form-group">
-                                                <input type="email" className="form-control" name="email" placeholder="Email" onChange={handleInputChange}/>
-                                            </div>
-                                        </div>
+            <label>Email</label>
+            <input type="email"></input>
 
-                                        <div className="col-md-12">
-                                            <div className="form-group">
-                                                <input type="text" className="form-control" name="subject" placeholder="Subject" onChange={handleInputChange}/>
-                                            </div>
-                                        </div>
+            <label>Subject</label>
+            <input type="text"></input>
 
-                                        <div className="col-md-12">
-                                            <div className="form-group">
-                                                <textarea type="text" className="form-control" name="message" placeholder="Message" cols="30" rows="6" onChange={handleInputChange}></textarea>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-12">
-                                            <div className="form-group">
-                                                <input type="submit" value="Send Message" className="bTNN"/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                        <div className="col-md-6 d-flex align-items-stretch">
-                            <div className="info-wrap w-100 p-lg-5 p-4 img">
-                                <h2>Contact Me</h2>
-                                <p className="mb-4">Open for any suggestions</p>
-                                <div className="dbox w-100 d-flex align-items-center">
-                                    <div className="icon d-flex align-items-center justify-content-center">
-                                        <span className="fa fa-map-marker"></span>
-                                    </div>
-                                    <div className="text pl-3">
-                                        <p>
-                                            <span>Address:</span>
-                                            <a>Langa, Cape Town</a> 
-                                        </p>
-                                    </div>
-                                </div>
-
-                                <div className="dbox w-100 d-flex align-items-center">
-                                    <div className="icon d-flex align-items-center justify-content-center">
-                                        <span className="fa fa-phone"></span>
-                                    </div>
-                                    <div className="text pl-3">
-                                        <p>
-                                            <span>Phone:</span>
-                                            <a href="tel://0733265346">+27 733 265 346</a>
-                                        </p>
-                                    </div>
-                                </div>
-
-                                <div className="dbox w-100 d-flex align-items-center">
-                                    <div className="icon d-flex align-items-center justify-content-center">
-                                        <span className="fa fa-paper-plane"></span>
-                                    </div>
-                                    <div className="text pl-3">
-                                        <p>
-                                            <span>Email:</span>
-                                            <a href="mailto:craxoti@gmail.com">craxoti60@gmail.com</a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+            <label>Message</label>
+            <textarea rows="6" placeholder="Type your message here" />
+            <button className="bTNN">Submit</button>
+        </form>
+    </div>
   )
 }
 

30124fc28467f206fd500f6533c6b22a1084ff86
Chuma-rxt <craxoti60@gmail.com>
2024-02-12 13:34:43 +0200

    added my first slid

--- src/components/Home1.js
+++ src/components/Home1.js
@@ -1,9 +1,12 @@
 import "./Home1Styles.css";
 
 import React from 'react'
-import Cis from "../assets/Cisco-Courses.jpg"
+import Cis from "../assets/Cisco-Courses.jpg";
+import { NavLink } from "react-router-dom";
 
-const Home1 = () => {
+function Home1(props) {
+    const { someProp } = props;
+
   return (
     <div className="slide-container">
         <div className="slide-content">
@@ -13,8 +16,17 @@ const Home1 = () => {
                         <span className="overlay"></span>
 
                         <div className="card-image">
-                            <img src={Cis} className="img-card" alt="true" />
+                            <img src={Cis} className="card-img" alt="true" />
                         </div>
+                    </div>
+                    <div className="card-content">
+                        <h2 className="name"> Communication Networks</h2>
+                        <p className="description">Completed the course CCNAv7: Switching, Routing and Wireless Essentials course administered by the undersigned instructor.</p>
+
+                        <div className="pro-btn">
+                            <NavLink to={props.view} className="bTn">VIEW</NavLink>
+                            <NavLink to={props.source} className="bTn">SOURCE</NavLink>
+                        </div>  
                     </div>
                 </div>
             </div>

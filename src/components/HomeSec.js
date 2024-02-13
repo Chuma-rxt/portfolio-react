7c49e6dd0197c80c1c36ea8a7596c13a05c4ca60
Chuma-rxt <craxoti60@gmail.com>
2024-02-13 09:48:20 +0200

    slider

--- src/components/HomeSec.js
+++ src/components/HomeSec.js
@@ -8,12 +8,45 @@ import ProjectCardData from './WorkCrad';
 import { Link } from "react-router-dom";
 
 function HomeSec () {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
   return (
     <div className="HomeSec">
-      <Slider>
-        
-      </Slider>
-        {ProjectCardData.map(item=>(
+      <Slider {...settings}>
+
+      {ProjectCardData.map(item=>(
         <div className="card">
             <div className="card-top">
               <img src={item.imgsrc} alt={item.title} />
@@ -25,8 +58,8 @@ function HomeSec () {
               <button>{item.source}</button>
             </div>
         </div>   
-        ))}
-        
+        ))}   
+      </Slider>   
     </div>
   );
 }

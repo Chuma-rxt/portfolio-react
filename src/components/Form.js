c8a85f0406cc1c90d5cba7d6b6f3463ae39ad302
Chuma-rxt <craxoti60@gmail.com>
2024-02-13 16:18:55 +0200

    app

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,22 +2,26 @@ import "./FormStyles.scss";
 
 import React from 'react';
 
-const Form = () => {
+function Form ()  {
+
+  const handleSubmit = () => {}
+
+  const handleInputChange = () => {}
   return (
     <div className="form">
         <form>
             <label>Your Name</label>
-            <input type="text"></input>
+            <input type="text" placeholder="Name" onChange={handleInputChange}></input>
 
             <label>Email</label>
-            <input type="email"></input>
+            <input type="email" placeholder="Email" onChange={handleInputChange}></input>
 
             <label>Subject</label>
-            <input type="text"></input>
+            <input type="text" placeholder="Subject" onChange={handleInputChange}></input>
 
             <label>Message</label>
             <textarea rows="6" placeholder="Type your message here" />
-            <button className="bTNN">Submit</button>
+            <button className="bTNN" onSubmit={handleSubmit}>Submit</button>
         </form>
     </div>
   )

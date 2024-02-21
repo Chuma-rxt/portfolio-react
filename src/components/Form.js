641ad709accdc5682f33f4ee2e56c1742fca2fdd
Chuma-rxt <craxoti60@gmail.com>
2024-02-21 08:48:47 +0200

    verifying recaptcha

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,6 @@ import ReCAPTCHA from 'react-google-recaptcha';
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import emailjs from 'emailjs-com'; // Import emailjs-com
-import fireDb from "./firebaseInit"; // Import initialized Firebase app
 import { MdLocationOn, MdPhone, MdEmail, MdLanguage } from 'react-icons/md';
 import "./FormStyles.scss";
 
@@ -14,40 +13,30 @@ function Form() {
         Message: ''
     });
 
+    const [captchaVerified, setCaptchaVerified] = useState(false);
+
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setUser({ ...user, [name]: value });
     }
 
     const handleCaptchaChange = (response) => {
-        // Assuming 'response' is the captcha response token
-        // Validate the captcha response
-        if (isValidCaptcha(response)) {
-            // Captcha validation passed, proceed with further actions
-            console.log("Captcha validation passed. Proceeding with further actions.");
-            // Add your logic here
+        if (response) {
+            setCaptchaVerified(true);
         } else {
-            // Captcha validation failed
-            console.error("Captcha validation failed. Please try again.");
-            // Handle failure case or prompt the user to try again
+            setCaptchaVerified(false);
         }
     }
 
-    const isValidCaptcha = (response) => {
-        // You can implement your captcha validation logic here
-        // For now, return true to simulate a successful validation
-        return true;
-    }
-
     const isFormValid = () => {
-        return user.FullName.trim() !== '' && user.Email.trim() !== '' && user.Message.trim() !== '';
+        return user.FullName.trim() !== '' && user.Email.trim() !== '' && user.Message.trim() !== '' && captchaVerified;
     }
 
-    const getData = async (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         if (!isFormValid()) {
-            toast.error('Please fill in all fields');
+            toast.error('Please fill in all fields and verify captcha');
             return;
         }
 
@@ -81,28 +70,24 @@ function Form() {
     }
 
     return (
-       
-        
-        <div className="form" fadeIn>
-
-        <div className="contact-info" fadeIn>
-            <h2>Contact Information</h2>
-            <ul>
-                <li><MdLocationOn /> <strong>Location:</strong> Langa, Cape Town, 7441</li>
-                <li><MdPhone /> <strong>Phone:</strong> +27 733 265 346</li>
-                <li><MdEmail /> <strong>Email:</strong> <a href="mailto:craxoti60@gmail.com">craxoti60@gmail.com</a></li>
-                <li><MdLanguage /> <strong>Website:</strong> <a href="https://portfolio-rose-mu-47.vercel.app" target="_blank" rel="noopener noreferrer">My Portfolio</a></li>
-            </ul>
+        <div className="form">
+            <div className="contact-info">
+                <h2>Contact Information</h2>
+                <ul>
+                    <li><MdLocationOn /> <strong>Location:</strong> Langa, Cape Town, 7441</li>
+                    <li><MdPhone /> <strong>Phone:</strong> +27 733 265 346</li>
+                    <li><MdEmail /> <strong>Email:</strong> <a href="mailto:craxoti60@gmail.com">craxoti60@gmail.com</a></li>
+                    <li><MdLanguage /> <strong>Website:</strong> <a href="https://portfolio-rose-mu-47.vercel.app" target="_blank" rel="noopener noreferrer">My Portfolio</a></li>
+                </ul>
             </div>
-            <div class="left-align">
+
+            <div className="left-align">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28486.118852609193!2d18.4240555!3d-33.9248688!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc670e6b8b7a79%3A0x23a590251013b17d!2sCape%20Town%2C%20South%20Africa!5e0!3m2!1sen!2suk!4v1645393407367!5m2!1sen!2suk" width="500" height="600" allowfullscreen="" loading="lazy"></iframe>
             </div>
 
+            <form onSubmit={handleSubmit}>
+                <h2 style={{ textAlign: "center", color: "#595959" }}>Send Me A Message</h2>
 
-            <form onSubmit={getData}>
-                <h2  style={{ textAlign: "center", color: "#595959" }}>Send Me A Message</h2>
-
-                
                 <label>Your Name</label>
                 <input
                     type="text"
@@ -111,7 +96,7 @@ function Form() {
                     value={user.FullName}
                     onChange={handleInputChange}
                     required
-                    pattern="[a-zA-Z]{3,}" 
+                    pattern="[a-zA-Z]{3,}"
                     title="Name must be at least 3 characters long and contain only letters"
                 />
 
@@ -141,7 +126,6 @@ function Form() {
                         className="my-recaptcha"
                         sitekey="6LeKb3IpAAAAAN_9K8pJxKSMuSCD1HyKlQLBaSJC"
                         onChange={handleCaptchaChange}
-                        
                     />
                 </div>
 
@@ -149,7 +133,6 @@ function Form() {
             </form>
             <ToastContainer />
         </div>
-        
     );
 }
 

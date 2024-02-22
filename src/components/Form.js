47df91dbe4699f6e9425244d2b143d217e030462
Chuma-rxt <craxoti60@gmail.com>
2024-02-22 11:29:39 +0200

    responsiveness

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,6 +4,7 @@ import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import emailjs from 'emailjs-com'; // Import emailjs-com
 import { MdLocationOn, MdPhone, MdEmail, MdLanguage } from 'react-icons/md';
+//import { FaGithub, FaCodepen, FaTwitter, FaInstagram } from 'react-icons/fa';
 import "./FormStyles.scss";
 
 function Form() {
@@ -82,70 +83,93 @@ function Form() {
                 </ul>
             </div>
 
-            <div className="left-align">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28486.118852609193!2d18.4240555!3d-33.9248688!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc670e6b8b7a79%3A0x23a590251013b17d!2sCape%20Town%2C%20South%20Africa!5e0!3m2!1sen!2suk!4v1645393407367!5m2!1sen!2suk" width="500" height="600" allowfullscreen="" loading="lazy"></iframe>
-            </div>
-
-            <form onSubmit={handleSubmit}>
-                <h2 style={{ textAlign: "center", color: "#595959" }}>Send Me A Message</h2>
-
-                <label>Your Name</label>
-                <input
-                    type="text"
-                    name="FullName"
-                    placeholder="Name"
-                    value={user.FullName}
-                    onChange={handleInputChange}
-                    required
-                    pattern="[a-zA-Z]{2,}"
-                    title="Name must be at least 3 characters long and contain only letters"
-                />
-
-                <label>Subject</label>
-                <input
-                    type="text"
-                    name="Subject"
-                    placeholder="subject"
-                    value={user.Subject}
-                    onChange={handleInputChange}
-                    required
-                    pattern="[a-zA-Z]{3,}"
-                    
-                />
-
-                <label>Email</label>
-                <input
-                    type="email"
-                    name="Email"
-                    placeholder="Email"
-                    value={user.Email}
-                    onChange={handleInputChange}
-                    required
-                    pattern="[a-z0-9._%+-]+@gmail\.com"
-                />
-
-                <label>Message</label>
-                <textarea
-                    rows="6"
-                    name="Message"
-                    placeholder="Type your message here"
-                    value={user.Message}
-                    onChange={handleInputChange}
-                    required
-                />
-
-                <div className="send-btn-container">
-                    <ReCAPTCHA
-                        className="my-recaptcha"
-                        sitekey="6LeKb3IpAAAAAN_9K8pJxKSMuSCD1HyKlQLBaSJC"
-                        onChange={handleCaptchaChange}
-                    />
+            <div class="form-row">
+                <div className="iframe-container">
+                    <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28486.118852609193!2d18.4240555!3d-33.9248688!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc670e6b8b7a79%3A0x23a590251013b17d!2sCape%20Town%2C%20South%20Africa!5e0!3m2!1sen!2suk!4v1645393407367!5m2!1sen!2suk"
+                    title="Google Maps"
+                    allowFullScreen
+                    loading="lazy"
+                    ></iframe>
                 </div>
 
-                <button type="submit" className="bTNN">Submit</button>
-            </form>
+                <form onSubmit={handleSubmit}>
+                    <h2 style={{ textAlign: "center", color: "#595959" }}>Send Me A Message</h2>
+                    <br/>
+
+                    <label>Your Name</label>
+                    <input
+                        type="text"
+                        name="FullName"
+                        placeholder="Name"
+                        value={user.FullName}
+                        onChange={handleInputChange}
+                        required
+                        pattern="[a-zA-Z]{2,}"
+                        title="Name must be at least 3 characters long and contain only letters"
+                    />
+
+                    <label>Subject</label>
+                    <input
+                        type="text"
+                        name="Subject"
+                        placeholder="subject"
+                        value={user.Subject}
+                        onChange={handleInputChange}
+                        required
+                        pattern="[a-zA-Z]{3,}"
+                        
+                    />
+
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        name="Email"
+                        placeholder="Email"
+                        value={user.Email}
+                        onChange={handleInputChange}
+                        required
+                        pattern="[a-z0-9._%+-]+@gmail\.com"
+                    />
+
+                    <label>Message</label>
+                    <textarea
+                        rows="6"
+                        name="Message"
+                        placeholder="Type your message here"
+                        value={user.Message}
+                        onChange={handleInputChange}
+                        required
+                    />
+
+                    <div className="send-btn-container">
+                        <ReCAPTCHA
+                            className="my-recaptcha"
+                            sitekey="6LeKb3IpAAAAAN_9K8pJxKSMuSCD1HyKlQLBaSJC"
+                            onChange={handleCaptchaChange}
+                        />
+                    </div>
+                    <br/><br/><br/>
+                    <button type="submit" className="bTNN">Submit</button>
+                </form>
+            </div>
             <ToastContainer />
         </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    
+        
+  
     );
 }
 
